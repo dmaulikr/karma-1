70df823e1fc865e1bedf41ef2ede2e90ad39b46d
Jessica Ji <jji@berkeley.edu>
2016-04-02 17:50:53 -0700

    edits

--- Karma/Karma/SignUpViewController.swift
+++ Karma/Karma/SignUpViewController.swift
@@ -19,33 +19,82 @@ class SignUpViewController: UIViewController {
     
     @IBOutlet weak var takenAlert: UILabel!
     @IBOutlet weak var emailAlert: UILabel!
+    @IBOutlet weak var sameEmail: UILabel!
+    
+    var duplicateUsers = []
+    var duplicateEmails = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         takenAlert.hidden = true
         emailAlert.hidden = true
+        sameEmail.hidden = true
+    }
+    
+    func displayEmptyFieldError() {
+        let alert = UIAlertController(title: "halp", message: "Please fill in all fields", preferredStyle: .Alert)
+        let okay = UIAlertAction(title: "Ok", style: .Default, handler: { (action) -> Void in
+        })
+        alert.addAction(okay)
+        self.presentViewController(alert, animated: true, completion: nil)
     }
 
     
     @IBAction func trySignUp(sender: AnyObject) {
+        
+        self.emailAlert.hidden = true
+        self.takenAlert.hidden = true
         
         let user = PFUser()
         user.username = username.text
         user.password = password.text
         user.email = email.text
         user["firstName"] = firstName.text
+        
         user["lastName"] = lastName.text
         user["audienceLim"] = 1
         
+        var query = PFQuery(className: "_User")
+        query.whereKey("username", equalTo: self.username.text!)
+        do {
+            self.duplicateUsers = try query.findObjects()
+        } catch {
+            print("Weird error")
+        }
+        
+        query = PFQuery(className: "_User")
+        query.whereKey("email", equalTo: self.email.text!)
+        do {
+            self.duplicateEmails = try query.findObjects()
+        } catch {
+            print("Weird error")
+        }
+        
+        print(self.duplicateUsers.count)
+        print(self.duplicateEmails.count)
+
+        
+        if self.duplicateUsers.count > 0 {
+            self.takenAlert.hidden = false
+            self.duplicateUsers = []
+        } else {
+        
         user.signUpInBackgroundWithBlock { (success, error) -> Void in
-            //query
-            
-            if (success) {
-                self.performSegueWithIdentifier("toMain2", sender: self)
-            } else {
+            if (self.username.text == "" || self.password.text == "" || self.email.text == "" || self.lastName.text == "" || self.firstName.text == "") {
                 
-                self.takenAlert.hidden = false
+                self.displayEmptyFieldError()
+                
+            } else {
+                    if (success) {
+                        self.performSegueWithIdentifier("toMain2", sender: self)
+                    } else {
+                        print("email error?")
+                        self.emailAlert.hidden = false
+                    }
+                }
             }
+        
+        
         }
     }
     override func didReceiveMemoryWarning() {
