833a5be6a6d6914b47128cec9b9addf177d7df48
Jessica Ji <jji@berkeley.edu>
2016-04-24 02:32:55 -0700

    merge

--- Karma/Karma/AppDelegate.swift
+++ Karma/Karma/AppDelegate.swift
@@ -30,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         });
         
         Parse.initializeWithConfiguration(config);
-
+        DropDown.startListeningToKeyboard()
         
         
         return true
