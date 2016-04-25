6f4b83a359c3cc17a862104db95fe3d85cc8b14e
Shaan Appel <shaan.appel@yahoo.com>
2016-04-24 18:14:39 -0700

    stuff

--- Karma/Karma/receivedViewController.swift
+++ Karma/Karma/receivedViewController.swift
@@ -132,6 +132,8 @@ class receivedViewController: UIViewController, UICollectionViewDelegate, UIColl
         receivedMessagesCollectionView.dataSource = self
         receivedMessagesCollectionView.delegate = self
         receivedMessagesCollectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        
+        
         self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
         self.navigationController?.navigationBar.translucent = false;
         //UIColor(red: 0.965, green: 0.698, blue: 0.42, alpha: 1)
@@ -147,7 +149,6 @@ class receivedViewController: UIViewController, UICollectionViewDelegate, UIColl
         
         let screenWidth = screenSize.width
         
-        self.view.backgroundColor = UIColor.greenColor()
         receivedMessagesCollectionView.frame.size.width = screenWidth
         receivedMessagesCollectionView.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0)
         
@@ -184,85 +185,40 @@ class receivedViewController: UIViewController, UICollectionViewDelegate, UIColl
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = receivedMessagesCollectionView.dequeueReusableCellWithReuseIdentifier("recCell", forIndexPath: indexPath) as!receivedMessageCollectionViewCell
         
-        //modify the cell
-        cell.backgroundColor = UIColor.whiteColor();
-        //255, 184, 77
         cell.backgroundView = nil;
-        print(body);
+        
+        
+        //Fill Cell Data
         cell.message.text = body[indexPath.row]
         cell.time.text = cleanTime(timesArray[indexPath.row])
         cell.location.text = locations[indexPath.row]
         
         cell.replyTextField.hidden = true
-        cell.frame.size.height = collectionView.frame.height / 5
-        cell.frame.origin.y = ((collectionView.frame.height / 5) + 10) * (CGFloat(indexPath.row))
-        /*if currentIndex == indexPath.row{
-            cell.frame.size.height = (collectionView.frame.height / 5) + 50
-            
-            
-            cell.replyTextField.hidden = !cell.replyTextField.hidden
-            
-            /*var frameRect = cell.replyTextField.frame;
-            frameRect.size.height = 50; // <-- Specify the height you want here.
-            frameRect.size.width = cell.frame.size.width - 10.0;
-            cell.replyTextField.bounds.origin.x = 5.0
-            cell.replyTextField.bounds.origin.y = (cell.message.bounds.origin.y + cell.message.frame.height)
-            cell.replyTextField.frame.origin.x = 5.0
-            cell.replyTextField.frame.origin.y = (cell.message.bounds.origin.y + cell.message.frame.height)
-            cell.replyTextField.layer.frame.origin.y = (cell.message.bounds.origin.y + cell.message.frame.height)
-            var layerFrame = cell.replyTextField.layer.frame
-            layerFrame.size.height = 50
-            layerFrame.size.width = cell.frame.size.width - 10.0;
-            
-            cell.replyTextField.frame = frameRect
-            cell.replyTextField.layer.frame = layerFrame*/
-            var frame = cell.replyTextField.frame
-            frame.size.height = 100.0;
-            cell.replyTextField.frame = frame
-            
-            
-            
-            cell.replyTextField.placeholder = "How would you like to reply?"
-            //cell.replyTextField.backgroundColor = UIColor.whiteColor()
-            
-            cell.replyTextField.borderStyle = UITextBorderStyle.Line
-        } else if currentIndex < indexPath.row  && currentIndex != -1{
-            cell.frame.origin.y = cell.frame.origin.y + 50
-        }*/
         
-        let collectionViewWidth = receivedMessagesCollectionView.bounds.size.width
-        cell.frame.size.width = collectionViewWidth
-        cell.frame.origin.x = receivedMessagesCollectionView.frame.origin.x
-        
-        
+        //design cell
+        cell.backgroundColor = UIColor.whiteColor();
         cell.layer.borderColor = UIColor.whiteColor().CGColor
         cell.layer.borderWidth = 1
-        
+        cell.layer.shadowOffset = CGSizeMake(0, 1)
         cell.layer.shadowColor = UIColor(netHex:0xCDBA96).CGColor
         cell.layer.shadowRadius = 3
         cell.layer.cornerRadius = 3
         
         cell.layer.shadowOpacity = 0.7
         
-        cell.layer.shadowOffset = CGSizeMake(0, 3)
+        let shadowFrame: CGRect = (cell.layer.bounds)
+        let shadowPath: CGPathRef = UIBezierPath(rect: shadowFrame).CGPath
+        cell.layer.shadowPath = shadowPath
+        cell.clipsToBounds = false
         
+        
+        //reply button
         cell.replyButton.titleLabel?.font = UIFont.fontAwesomeOfSize(10)
         cell.replyButton.setTitle("Reply", forState: .Normal)
         
         if replies[indexPath.row] != "" {
             cell.replyButton.setTitle(String.fontAwesomeIconWithName(.Check), forState: .Normal)
         }
-        
-        //cell.replyButton.setTit
-        cell.replyButton.layer.cornerRadius = 0.5 *  cell.replyButton.bounds.size.width
-        cell.replyButton.clipsToBounds = true
-        
-        // Maybe just me, but I had to add it to work:
-        cell.clipsToBounds = false
-        
-        let shadowFrame: CGRect = (cell.layer.bounds)
-        let shadowPath: CGPathRef = UIBezierPath(rect: shadowFrame).CGPath
-        cell.layer.shadowPath = shadowPath
         
         
         
@@ -271,19 +227,6 @@ class receivedViewController: UIViewController, UICollectionViewDelegate, UIColl
         return cell
     }
     
-    
-    
-//    @IBAction func replyButtonClicked(sender: AnyObject) {
-//        if let button = sender as? UIButton {
-//            if let superview = button.superview {
-//                if let cell = superview.superview as? FeedCollectionViewCell {
-//                    let indexPath = collectionView?.indexPathForCell(cell)
-//                    currentIndex = indexPath!.row
-//                    collectionView?.reloadData()
-//                }
-//            }
-//        }
-//    }
 
     @IBAction func replyButtonClicked(sender: AnyObject) {
         if let button = sender as? UIButton {
@@ -301,10 +244,12 @@ class receivedViewController: UIViewController, UICollectionViewDelegate, UIColl
         }
     }
     
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        
+        return UIEdgeInsetsMake(10, -30, 0, -30) // margin between cells
+    }
 
 
-    
-    
     
         
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
