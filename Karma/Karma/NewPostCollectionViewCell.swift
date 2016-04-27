7ab62020b6808f61253ceee47cf4c75c4fff50b4
Shaan Appel <shaan.appel@yahoo.com>
2016-04-27 00:17:36 -0700

    asdbaksd

--- Karma/Karma/NewPostCollectionViewCell.swift
+++ Karma/Karma/NewPostCollectionViewCell.swift
@@ -29,6 +29,7 @@ class NewPostCollectionViewCell: UICollectionViewCell, UITextViewDelegate, UIPop
     var usersInRange = Array<PFObject>()
     
     var delegate:NewPostCollectionViewDelegate? = nil
+    var canHitSend = true
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -81,103 +82,111 @@ class NewPostCollectionViewCell: UICollectionViewCell, UITextViewDelegate, UIPop
         delegate!.selectLocationsPressed(self)
     }
     @IBAction func sendMessage(sender: AnyObject) {
-        print(textView.text)
         
-        if (textView.text != "" || textView.text != "What's on your mind?") {
-            currUser?.incrementKey("audienceLim", byAmount: 0.2)
-            currUser!.saveInBackgroundWithBlock {
-                (success: Bool, error: NSError?) -> Void in
-                if (success) {
-                    // The object has been saved.
-                } else {
-                    // There was a problem, check error.description
+        if canHitSend {
+            print(textView.text)
+            canHitSend = false;
+            
+            if (textView.text != "" || textView.text != "What's on your mind?") {
+                currUser?.incrementKey("audienceLim", byAmount: 0.2)
+                currUser!.saveInBackgroundWithBlock {
+                    (success: Bool, error: NSError?) -> Void in
+                    if (success) {
+                        // The object has been saved.
+                    } else {
+                        // There was a problem, check error.description
+                    }
                 }
-            }
-            let msg = PFObject(className: "Messages")
-            msg["messageBody"] = textView.text
-            
-            if (currUser!["location"] == nil) {
-                delegate!.parentShouldShowAlert("We cannot find your location", title: "No Location")
-                return;
-            }
-            
-            msg["sentLocation"] = currUser!["location"] as! PFGeoPoint
-            msg["senderId"] = currUser!.objectId
-            msg["sentDate"] = NSDate()
-            msg["authorized"] = false
-            msg["flagged"] = false
-            msg["favorited"] = false
-            if (DataStorage.getDouble("radius") == 20.0) {
-                msg["sentScale"] = "Local"
-            } else if (DataStorage.getDouble("radius") == 200.0) {
-                msg["sentScale"] = "Mid-Range"
-            } else {
-                msg["sentScale"] = "Expansive"
-            }
-            
-            //            msg["audience"] = selectedAudience
-            
-            //Edit once approved:
-            //msg["readIds"] = Array<ObjectIds>
-            //msg["replyText"] = String
-            
-            let recieverIds = NSMutableArray()
-            let recievedLocations = NSMutableArray()
-            
-            let userGeoPoint = currUser!["location"] as! PFGeoPoint
-            
-            let query = PFQuery(className:"_User")
-            print(DataStorage.getDouble("radius"))
-            query.whereKey("location", nearGeoPoint:userGeoPoint, withinMiles: DataStorage.getDouble("radius"))
-            query.limit = currUser!["audienceLim"] as! Int
-            print(currUser!["audienceLim"] as! Int)
-            query.findObjectsInBackgroundWithBlock {
-                (objects: [PFObject]?, error: NSError?) -> Void in
+                let msg = PFObject(className: "Messages")
+                msg["messageBody"] = textView.text
                 
-                if error == nil {
-                    if let objects = objects {
-                        if objects.count == 0 {
-                            // display alert
-                            self.delegate!.parentShouldShowAlert("Please select a larger range to pay the posivity forward!", title: "No Users in Range")
-                        } else {
-                            for object in objects {
-                                if (object.objectId != self.currUser?.objectId) {
-                                    recieverIds.addObject(object.objectId!)
-                                    recievedLocations.addObject(object["location"])
+                if (currUser!["location"] == nil) {
+                    delegate!.parentShouldShowAlert("We cannot find your location", title: "No Location")
+                    return;
+                }
+                
+                msg["sentLocation"] = currUser!["location"] as! PFGeoPoint
+                msg["senderId"] = currUser!.objectId
+                msg["sentDate"] = NSDate()
+                msg["authorized"] = false
+                msg["flagged"] = false
+                msg["favorited"] = false
+                if (DataStorage.getDouble("radius") == 20.0) {
+                    msg["sentScale"] = "Local"
+                } else if (DataStorage.getDouble("radius") == 200.0) {
+                    msg["sentScale"] = "Mid-Range"
+                } else {
+                    msg["sentScale"] = "Expansive"
+                }
+                
+                //            msg["audience"] = selectedAudience
+                
+                //Edit once approved:
+                //msg["readIds"] = Array<ObjectIds>
+                //msg["replyText"] = String
+                
+                let recieverIds = NSMutableArray()
+                let recievedLocations = NSMutableArray()
+                
+                let userGeoPoint = currUser!["location"] as! PFGeoPoint
+                
+                let query = PFQuery(className:"_User")
+                print(DataStorage.getDouble("radius"))
+                query.whereKey("location", nearGeoPoint:userGeoPoint, withinMiles: DataStorage.getDouble("radius"))
+                query.limit = currUser!["audienceLim"] as! Int
+                print(currUser!["audienceLim"] as! Int)
+                query.findObjectsInBackgroundWithBlock {
+                    (objects: [PFObject]?, error: NSError?) -> Void in
+                    
+                    if error == nil {
+                        if let objects = objects {
+                            if objects.count == 0 {
+                                // display alert
+                                self.delegate!.parentShouldShowAlert("Please select a larger range to pay the posivity forward!", title: "No Users in Range")
+                            } else {
+                                for object in objects {
+                                    if (object.objectId != self.currUser?.objectId) {
+                                        recieverIds.addObject(object.objectId!)
+                                        recievedLocations.addObject(object["location"])
+                                    }
                                 }
-                            }
-                            msg["recieverIds"] = recieverIds
-                            msg["recievedLocations"] = recievedLocations
-                            
-                            
-                            msg.saveInBackgroundWithBlock {
-                                (success: Bool, error: NSError?) -> Void in
-                                self.setPlaceholder()
-                                self.textView.endEditing(true)
-                                if (success) {
-                                    print("yaaaaas")
-                                    // if tap outside then shrink the box
-                                    // but if inside then expand and show the button
-                                } else {
-                                    print("error saving")
-                                    // display what kind of error?
+                                msg["recieverIds"] = recieverIds
+                                msg["recievedLocations"] = recievedLocations
+                                
+                                
+                                msg.saveInBackgroundWithBlock {
+                                    (success: Bool, error: NSError?) -> Void in
+                                    self.setPlaceholder()
+                                    self.textView.endEditing(true)
+                                    if (success) {
+                                        print("yaaaaas")
+                                        self.canHitSend = true;
+                                        var sendString = "Your message was succesfully sent to " + String(self.currUser!["audienceLim"] as! Int)
+                                        sendString = sendString + " people!"
+                                        self.delegate!.parentShouldShowAlert(sendString, title: "Sent!")
+                                        // if tap outside then shrink the box
+                                        // but if inside then expand and show the button
+                                    } else {
+                                        print("error saving")
+                                        // display what kind of error?
+                                    }
                                 }
                             }
                         }
+                    } else {
+                        print("Error: \(error!) \(error!.userInfo)")
                     }
-                } else {
-                    print("Error: \(error!) \(error!.userInfo)")
                 }
+                UIView.animateWithDuration(1.0, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+                    self.sentLabel.alpha = 1.0
+                    }, completion: nil)
+                UIView.animateWithDuration(1.0, delay: 1.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+                    self.sentLabel.alpha = 0.0
+                    }, completion: {
+                        (finished: Bool) -> Void in
+                })
+                
             }
-            UIView.animateWithDuration(1.0, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
-                self.sentLabel.alpha = 1.0
-                }, completion: nil)
-            UIView.animateWithDuration(1.0, delay: 1.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
-                self.sentLabel.alpha = 0.0
-                }, completion: {
-                    (finished: Bool) -> Void in
-            })
-            
         }
     }
     
